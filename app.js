17b49c61e121f416920fe044479e3f23ee0bb9a7
jleonen <jleonen2012@gmail.com>
2021-10-16 22:01:02 -0700

    Added function to check if email is valid or not

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const email = document.querySelector(".inputValue");
+const errorMsg = document.querySelector(".error-msg");
+
+errorMsg.style.display = "none";
+let errorCheck = false;
+
+const submitEmail = function () {
+  if (
+    (!email.value.includes("@") && !errorCheck) ||
+    (!email.value.includes(".com") && !errorCheck)
+  ) {
+    errorMsg.style.display = "block";
+    email.style.backgroundImage = "url('/images/icon-error.svg')";
+    email.style.backgroundRepeat = "no-repeat";
+    email.style.backgroundPosition = "right";
+    email.style.backgroundPositionX = "325px";
+    email.style.borderColor = "red";
+  } else {
+    errorMsg.style.display = "none";
+    email.style.backgroundImage = "none";
+    email.style.borderColor = "hsl(0, 36%, 70%)";
+  }
+};
