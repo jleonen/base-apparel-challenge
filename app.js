0d5ddc1933b6b6b912b09ed85ed1cc4c3f53fd98
jleonen <jleonen2012@gmail.com>
2021-10-17 01:50:16 -0700

    Implemented a success message to notify user that they have been successfully registered. Also implemented the logo as well.

--- app.js
+++ app.js
@@ -2,24 +2,40 @@
 
 const email = document.querySelector(".inputValue");
 const errorMsg = document.querySelector(".error-msg");
+const description = document.querySelector(".description");
+const submitBtn = document.querySelector("button");
+const heading = document.querySelector("h1");
 
 errorMsg.style.display = "none";
 let errorCheck = false;
+
+const renderError = function () {
+  errorMsg.style.display = "block";
+  email.style.backgroundImage = "url('/images/icon-error.svg')";
+  email.style.backgroundRepeat = "no-repeat";
+  email.style.backgroundPosition = "right";
+  email.style.backgroundPositionX = "325px";
+  email.style.borderColor = "red";
+};
+
+const successMsg = function () {
+  errorMsg.style.display = "none";
+  email.style.display = "none";
+  submitBtn.style.display = "none";
+  email.style.backgroundImage = "none";
+  email.style.borderColor = "hsl(0, 36%, 70%)";
+
+  heading.innerHTML = "Success!";
+  description.innerHTML = `An email has been sent to <a href="#">${email.value}</a>. Please check your email in a few minutes for a verification email. Click <a href="/">here</a> to go back to the homepage.`;
+};
 
 const submitEmail = function () {
   if (
     (!email.value.includes("@") && !errorCheck) ||
     (!email.value.includes(".com") && !errorCheck)
   ) {
-    errorMsg.style.display = "block";
-    email.style.backgroundImage = "url('/images/icon-error.svg')";
-    email.style.backgroundRepeat = "no-repeat";
-    email.style.backgroundPosition = "right";
-    email.style.backgroundPositionX = "325px";
-    email.style.borderColor = "red";
+    renderError();
   } else {
-    errorMsg.style.display = "none";
-    email.style.backgroundImage = "none";
-    email.style.borderColor = "hsl(0, 36%, 70%)";
+    successMsg();
   }
 };
